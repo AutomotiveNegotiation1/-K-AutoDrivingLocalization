fb53988cf7bf99b0c1ccacdd391a597c0af2ff8a
Byoungman An <bman@keti.re.kr>
2022-11-07 12:04:37 +0900

    [KETI] implement keti algorithm of positining (matlab to c)

---
+++ src/positioning/ros/devices/ros_test/include/ros_test/ros_test_terminate.h
@@ -0,0 +1,27 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: ros_test_terminate.h
+//
+// MATLAB Coder version            : 5.5
+// C/C++ source code generated on  : 22-Sep-2022 11:50:58
+//
+
+#ifndef ROS_TEST_TERMINATE_H
+#define ROS_TEST_TERMINATE_H
+
+// Include Files
+#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+
+// Function Declarations
+extern void ros_test_terminate();
+
+#endif
+//
+// File trailer for ros_test_terminate.h
+//
+// [EOF]
+//
---
+++ src/positioning/ros/devices/ros_test/src/ros_test_terminate.cpp
@@ -0,0 +1,27 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: ros_test_terminate.cpp
+//
+// MATLAB Coder version            : 5.5
+// C/C++ source code generated on  : 22-Sep-2022 11:50:58
+//
+
+// Include Files
+#include "ros_test_terminate.h"
+
+// Function Definitions
+//
+// Arguments    : void
+// Return Type  : void
+//
+void ros_test_terminate()
+{
+}
+
+//
+// File trailer for ros_test_terminate.cpp
+//
+// [EOF]
+//
