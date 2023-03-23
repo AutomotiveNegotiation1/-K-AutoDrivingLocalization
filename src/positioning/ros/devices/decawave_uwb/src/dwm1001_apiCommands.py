7ee77ba78903724f12ab167817241b67949cc92b
Jaewon <lee35460@gmail.com>
2023-03-23 18:27:19 +0900

    uwb raw dataloade code style

--- src/positioning/ros/devices/decawave_uwb/src/dwm1001_apiCommands.py
+++ src/positioning/ros/devices/decawave_uwb/src/dwm1001_apiCommands.py
@@ -1,68 +1,185 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.8
 """ For more info on the documentation go to https://www.decawave.com/sites/default/files/dwm1001-api-guide.pdf
 """
 
-class DWM1001_API_COMMANDS:
-        DOUBLE_ENTER    = b'\r\r'   # ASCII char for double Enter
-        SINGLE_ENTER    = b'\r'     # ASCII char for single Enter
-        HELP            = b'?'      # Display help
-        QUIT            = b'quit'   # Quit API shell mode
-        GC              = b'gc'     # Clears GPIO pin
-        GG              = b'gg'     # Reads GPIO pin level
-        GS              = b'gs'     # Sets GPIO as output and sets its value
-        GT              = b'gt'     # Toggle GPIO(must be an output)
-        F               = b'f'      # Show free memory on the heap
-        PS              = b'ps'     # Show info about running threads
-        PMS             = b'pms'    # Show power managements tasks. IDL means that task is idle. USE means that task is allocated in the power management
-        RESET           = b'reset'  # reset the dev board
-        UT              = b'ut'     # Show device uptime
-        FRST            = b'frst'   # Factory reset
-        TWI             = b'twi'    # General purpose I2C/TWI read
-        AID             = b'aid'    # Read ACC device ID
-        AV              = b'av'     # Rad ACC values
-        LES             = b'les'    # Show distances to ranging anchors and the position if location engine is enabled
-        LEC             = b'lec'    # Show measurement and position in CSV format
-        LEP             = b'lep'    # Show position in CSV format.Sending this command multiple times will turn on/off this functionality.
-        SI              = b'si'     # System Info
-        NMG             = b'nmg'    # Get node mode info
-        NMO             = b'nmo'    # Enable passive offline option and resets the node
-        NMP             = b'nmp'    # Enable active offline option and resets the node.
-        NMA             = b'nma'    # Configures node to as anchor, active and reset the node.
-        NMI             = b'nmi'    # Configures node to as anchor initiator, active and reset the node.
-        NMT             = b'nmt'    # Configures node to as tag, active and reset the node
-        NMTL            = b'nmtl'   # Configures node to as tag, active, low power and resets the node.
-        BPC             = b'bpc'    # Toggle UWB bandwidth / tx power compensation.
-        LA              = b'la'     # Show anchor list
-        STG             = b'stg'    # Display statistics
-        STC             = b'stc'    # Clears statistics
-        TLV             = b'tlv'    # Parses given tlv frame, see section 4 for valid TLV commands
-        AURS            = b'aurs'   # Set position update rate. See section 4.3.3 for more detail.
-        AURG            = b'aurg'   # Get position update rate. See section 4.3.4 for more details
-        APG             = b'apg'    # Get position of the node.See section 3.4.2 for more detail
-        APS             = b'aps'    # Set position of the node.See section 3.4.2for more detail
-        ACAS            = b'acas'   # Configures node as anchor with given options
-        ACTS            = b'acts'   # Configures node as tag with given options
+import rospy, time
+
+
+class DWM1001ApiCommands:
+      
+        def __init__(self, instance, rate=10, verbose=False):                        
+                self.instance = instance
+                
+                self.verbose = verbose
+                self.rate = rospy.Rate(rate)
+                # serial initializing
+                self.instance.close()
+                
+                # sleep for one sec
+                time.sleep(1)
+                
+                # open serial port
+                self.instance.open()
+                
+                self.serialReadLine=None
+
+        def commands(self, input_command):
+                output_command = None                        
+                if input_command == 'DOUBLE_ENTER':
+                        output_command = b'\r\r'   # ASCII char for double Enter
+                elif input_command == 'SINGLE_ENTER':
+                        output_command = b'\r'     # ASCII char for single Enter        
+                elif input_command == 'HELP':
+                        output_command = b'?'      # Display help
+                elif input_command == 'QUIT':
+                        output_command = b'quit'   # Quit API shell mode
+                elif input_command == 'GC':
+                        output_command = b'gc'     # Clears GPIO pin
+                elif input_command == 'GG':
+                        output_command = b'gg'     # Reads GPIO pin level
+                elif input_command == 'GS':
+                        output_command = b'gs'     # Sets GPIO as output and sets its value
+                elif input_command == 'SINGLE_ENTER':
+                        output_command = b'gt'     # Toggle GPIO(must be an output)
+                elif input_command == 'F':
+                        output_command = b'f'      # Show free memory on the heap
+                elif input_command == 'PS':
+                        output_command = b'ps'     # Show info about running threads
+                elif input_command == 'PMS':
+                        output_command = b'pms'    # Show power managements tasks. IDL means that task is idle. USE means that task is allocated in the power management
+                elif input_command == 'RESET':
+                        output_command = b'reset'  # reset the dev board
+                elif input_command == 'UT':
+                        output_command = b'ut'     # Show device uptime
+                elif input_command == 'FRST':
+                        output_command = b'frst'   # Factory reset
+                elif input_command == 'TWI':
+                        output_command = b'twi'    # General purpose I2C/TWI read
+                elif input_command == 'AID':
+                        output_command = b'aid'    # Read ACC device ID
+                elif input_command == 'AV':
+                        output_command = b'av'     # Rad ACC values
+                elif input_command == 'LES':
+                        output_command = b'les'    # Show distances to ranging anchors and the position if location engine is enabled
+                elif input_command == 'LEC':
+                        output_command = b'lec'    # Show measurement and position in CSV format
+                elif input_command == 'LEP':
+                        output_command = b'lep'    # Show position in CSV format.Sending this command multiple times will turn on/off this functionality.
+                elif input_command == 'SI':
+                        output_command = b'si'     # System Info
+                elif input_command == 'NMG':
+                        output_command = b'nmg'    # Get node mode info
+                elif input_command == 'NMO':
+                        output_command = b'nmo'    # Enable passive offline option and resets the node
+                elif input_command == 'NMP':
+                        output_command = b'nmp'    # Enable active offline option and resets the node.
+                elif input_command == 'NMA':
+                        output_command = b'nma'    # Configures node to as anchor, active and reset the node.
+                elif input_command == 'NMI':
+                        output_command = b'nmi'    # Configures node to as anchor initiator, active and reset the node.
+                elif input_command == 'NMT':
+                        output_command = b'nmt'    # Configures node to as tag, active and reset the node
+                elif input_command == 'NMTL':
+                        output_command = b'nmtl'   # Configures node to as tag, active, low power and resets the node.
+                elif input_command == 'BPC':
+                        output_command = b'bpc'    # Toggle UWB bandwidth / tx power compensation.
+                elif input_command == 'LA':
+                        output_command = b'la'     # Show anchor list
+                elif input_command == 'STG':
+                        output_command = b'stg'    # Display statistics
+                elif input_command == 'STC':
+                        output_command = b'stc'    # Clears statistics
+                elif input_command == 'TLS':
+                        output_command = b'tlv'    # Parses given tlv frame, see section 4 for valid TLV commands
+                elif input_command == 'AURS':
+                        output_command = b'aurs'   # Set position update rate. See section 4.3.3 for more detail.
+                elif input_command == 'AURG':
+                        output_command = b'aurg'   # Get position update rate. See section 4.3.4 for more details
+                elif input_command == 'APG':   
+                        output_command = b'apg'    # Get position of the node.See section 3.4.2 for more detail
+                elif input_command == 'APS':
+                        output_command = b'aps'    # Set position of the node.See section 3.4.2for more detail
+                elif input_command == 'ACAS':
+                        output_command = b'acas'   # Configures node as anchor with given options
+                elif input_command == 'ACTS':
+                        output_command = b'acts'   # Configures node as tag with given options
+                self.instance.write(output_command)
+                if self.verbose == True:
+                        rospy.loginfo("DWM1001 API Commands > ", input_command)
+                
+                
+        def is_Open(self):
+                if (self.instance.isOpen()):
+                        if self.verbose == True:
+                                rospy.loginfo("Port opened: "+ str(self.instance.name))
+                        # start sending commands to the board so we can initialize the board
+                        self.initialize_DWM1001API()
+                        # give some time to DWM1001 to wake up
+                        time.sleep(2)
+                        # send command lec, so we can get positions is CSV format
+                        self.commands('LEC')
+                        self.commands('SINGLE_ENTER')
+                        if self.verbose == True:
+                                rospy.loginfo("Reading DWM1001 coordinates")
+                else:
+                        if self.verbose == True:
+                                rospy.loginfo("Can't open port: "+ str(self.serialPortDWM1001.name))
         
-        def set_position(self, arrayData, i):
-                x = float(arrayData[4+6*i])
-                y = float(arrayData[5+6*i])
-                z = float(arrayData[6+6*i])
-                return [x, y, z]
+        def initialize_DWM1001API(self):
+                if self.verbose == True:
+                        rospy.loginfo("Start DWM1001 API initializing...")
+                        
+                # reset incase previuos run didn't close properly
+                self.commands('RESET')
+                # send ENTER two times in order to access api
+                self.commands('SINGLE_ENTER')
+                # sleep for half a second
+                time.sleep(0.5)
+                self.commands('SINGLE_ENTER')
+                # sleep for half second
+                time.sleep(0.5)
+                # send a third one - just in case
+                self.commands('SINGLE_ENTER')
         
-        def set_node_id(self, arrayData, i):
-                node_id = arrayData[3+6*i]
+        def close(self):
+                return self.instance.close()
+        
+        def raw(self):
+                serialReadLine = self.instance.read_until().decode('utf-8')
+                return serialReadLine
+        
+        def read(self):
+                return self.raw(), self.parsed_data()
+        
+        def parsed_data(self):
+                data = [x.strip() for x in self.serialReadLine.strip().split(',')]
+                ANNum = int((len(data) - 7)/6)
+                anchor = self.AnchorParsed(ANNum, data)
+                tag = self.TagParsed(data)
+        
+        def AnchorParsed(self, ANNum, data):
+                identity = self.AnchorIdentity(ANNum, data)
+                pose = self.AnchorPose(ANNum, data)
                 
-                return node_id
+        def AnchorIdentity(self, ANNum, data):
+                ID = data[0]
+                ANSerial = [data[3+6*i] for i in range(ANNum)]
+                        
+        def AnchorPose(self, ANNum, data):
+                x = [data[4+6*i] for i in range(ANNum)]
+                y = [data[5+6*i] for i in range(ANNum)]
+                z = [data[6+6*i] for i in range(ANNum)]
+                distance = [data[7+6*i] for i in range(ANNum)]
+        
+        def TagParsed(self, data):
+                identity = self.TagIdentity(data)
+                pose = self.TagPose(data)
                 
-        def set_node_number(self, arrayData):
-                number_of_anchors = int((len(arrayData) - 7)/6)
+        def TagIdentity(self, data):
+                ID = data[-5]
+                TASerial = self.instance.name
                 
-                return number_of_anchors
-                
-        def set_arrayData(self, serialData):
-                serialData = [x.strip() for x in serialData.strip().split(',')]
-                
-                return serialData
-                
-        def set_range(self, arrayData, i):
-                range = float(arrayData[7+6*i])
+        def TagPose(self, data):
+                x = data[-4]
+                y = data[-3]
+                z = data[-2]
--- src/positioning/ros/devices/decawave_uwb/src/mdek1001_node.py
+++ src/positioning/ros/devices/decawave_uwb/src/mdek1001_node.py
@@ -9,10 +9,10 @@ from std_msgs.msg       import Float64
 from decawave_uwb.msg import uwb_anchor
 from decawave_uwb.msg import Ranging
 
-from dwm1001_apiCommands import DWM1001_API_COMMANDS
+from dwm1001_apiCommands import DWM1001ApiCommands
             
 
-class dwm1001_localizer:
+class DWM1001DataReader:
        
     def __init__(self, serial_instance) :
         """
@@ -21,7 +21,7 @@ class dwm1001_localizer:
 
         # Init node
         # rospy.init_node('DWM1001_Active_{}'.format(serial_instance.split("/")[2]), anonymous=False)
-        rospy.init_node('get_uwb_data', anonymous=True)
+        rospy.init_node('DWM1001/Data/Reader', anonymous=True)
 
         # Get port and tag name
         self.dwm_port = serial_instance.port
@@ -52,80 +52,58 @@ class dwm1001_localizer:
         :param:
         :returns: none
         """
-
-        # close the serial port in case the previous run didn't closed it properly
-        self.serialPortDWM1001.close()
-        # sleep for one sec
-        time.sleep(1)
-        # open serial port
-        self.serialPortDWM1001.open()
-
-        # check if the serial port is opened
-        if(self.serialPortDWM1001.isOpen()):
-            rospy.loginfo("Port opened: "+ str(self.serialPortDWM1001.name) )
-            # start sending commands to the board so we can initialize the board
-            self.initializeDWM1001API()
-            # give some time to DWM1001 to wake up
-            time.sleep(2)
-            # send command lec, so we can get positions is CSV format
-            self.serialPortDWM1001.write(DWM1001_API_COMMANDS.LEC)
-            self.serialPortDWM1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
-            rospy.loginfo("Reading DWM1001 coordinates")
-        else:
-            rospy.loginfo("Can't open port: "+ str(self.serialPortDWM1001.name))
+        DWM1001 = DWM1001ApiCommands(self.serialPortDWM1001, rata=10)
+        DWM1001.is_Open()
 
         try:
 
             while not rospy.is_shutdown():
                 # just read everything from serial port
-                serialReadLine = self.serialPortDWM1001.read_until().decode('utf-8')
+                raw,  parsed_data= DWM1001.read()
 
                 try:
-                    self.publishTagPositions(serialReadLine)
+                    self.publishTagPositions(parsed_data)
 
                 except IndexError:
                     rospy.loginfo("Found index error in the network array! DO SOMETHING!")
 
 
-
         except KeyboardInterrupt:
             rospy.loginfo("Quitting DWM1001 Shell Mode and closing port, allow 1 second for UWB recovery")
-            self.serialPortDWM1001.write(DWM1001_API_COMMANDS.RESET)
-            self.serialPortDWM1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
+            DWM1001.commands('RESET')
+            DWM1001.commands('SINGLE_ENTER')
 
         finally:
             rospy.loginfo("Quitting, and sending reset command to dev board")
             # self.serialPortDWM1001.reset_input_buffer()
-            self.serialPortDWM1001.write(DWM1001_API_COMMANDS.RESET)
-            self.serialPortDWM1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
+            DWM1001.commands('RESET')
+            DWM1001.commands('SINGLE_ENTER')
             self.rate.sleep()
-            serialReadLine = self.serialPortDWM1001.read_until()
-            if "reset" in serialReadLine:
+            raw,  parsed_data= DWM1001.read()
+            if "reset" in raw:
                 rospy.loginfo("succesfully closed ")
-                self.serialPortDWM1001.close()
+                DWM1001.close()
 
 
-    def publishTagPositions(self, serialData):
+    def publishTagPositions(self, parsed_data):
         """
         Publish anchors and tag in topics using Tag and Anchor Object
         :param networkDataArray:  Array from serial port containing all informations, tag xyz and anchor xyz
         :returns: none
         """
 
-        # arrayData = DWM1001_API_COMMANDS.set_arrayData(serialData)
-        arrayData = [x.strip() for x in serialData.strip().split(',')]
 
         # If getting a tag position
-        if "DIST" in arrayData[0] :
+        if "DIST" in parsed_data.anchor.identity.ID:
 
             # The number of elements should be 2 + 6*NUMBER_OF_ANCHORS + 5 (TAG POS)
             # number_of_anchors = DWM1001_API_COMMANDS.set_node_number(arrayData)
-            number_of_anchors = int((len(arrayData) - 7)/6)
+            number_of_anchors = parsed_data.anchor.ANNum
 
             for i in range(number_of_anchors) :
 
                 # node_id = DWM1001_API_COMMANDS.set_node_id(arrayData, i)
-                node_id = arrayData[3+6*i]
+                node_id = parsed_data.anchor.identity.ANserial
                 
                 first_time = False
                 if node_id not in self.topics :
@@ -149,12 +127,9 @@ class dwm1001_localizer:
                     p = PoseStamped()
                     p.header.stamp = rospy.Time.now()
                     p.header.frame_id = node_id
-                    # p.pose.position.x = DWM1001_API_COMMANDS.set_position(arrayData).x
-                    # p.pose.position.y = DWM1001_API_COMMANDS.set_position(arrayData).y
-                    # p.pose.position.z = DWM1001_API_COMMANDS.set_position(arrayData).z
-                    p.pose.position.x = float(arrayData[4+6*i])
-                    p.pose.position.y = float(arrayData[5+6*i])
-                    p.pose.position.z = float(arrayData[6+6*i])
+                    p.pose.position.x = parsed_data.anchor.pose.x[i]
+                    p.pose.position.y = parsed_data.anchor.pose.y[i]
+                    p.pose.position.z = parsed_data.anchor.pose.z[i]
                     p.pose.orientation.x = 0.0
                     p.pose.orientation.y = 0.0
                     p.pose.orientation.z = 0.0
@@ -167,8 +142,8 @@ class dwm1001_localizer:
                     dist.header.stamp = p.header.stamp
                     dist.anchorId = node_id
                     dist.tagId = self.tag_name
-                    # dist.range = DWM1001_API_COMMANDS.set_range(arrayData,i)
-                    dist.range = float(arrayData[7+6*i])
+                    dist.range = parsed_data.anchor.pose.distance[i]
+                    # dist.range = float(arrayData[7+6*i])
                     self.topics[node_id+"_dist"].publish(dist)
                 except :
                     pass
@@ -183,7 +158,7 @@ class dwm1001_localizer:
                                   + str(p.pose.position.z))
 
             # Now publish the position of the tag itself
-            if "POS" in arrayData[-5] :
+            if "POS" in parsed_data.tag.identity.ID:
 
                 # Topic is now a tag with same name as node_id
                 first_time = False
@@ -192,12 +167,9 @@ class dwm1001_localizer:
                     self.topics[self.tag_name] = rospy.Publisher('/dwm1001/tag/'+self.tag_name+"/position", PoseStamped, queue_size=100)
                 p = PoseStamped()
                 p.header.stamp = rospy.Time.now()  
-                # p.pose.position.x = DWM1001_API_COMMANDS.set_position(arrayData).x
-                # p.pose.position.y = DWM1001_API_COMMANDS.set_position(arrayData).y
-                # p.pose.position.z = DWM1001_API_COMMANDS.set_position(arrayData).z
-                p.pose.position.x = float(arrayData[-4])
-                p.pose.position.y = float(arrayData[-3])
-                p.pose.position.z = float(arrayData[-2])
+                p.pose.position.x = parsed_data.tag.pose.x
+                p.pose.position.y = parsed_data.tag.pose.y
+                p.pose.position.z = parsed_data.tag.pose.z
                 p.pose.orientation.x = 0.0
                 p.pose.orientation.y = 0.0
                 p.pose.orientation.z = 0.0
@@ -213,31 +185,12 @@ class dwm1001_localizer:
                                   + " z: "
                                   + str(p.pose.position.z))
 
-    def initializeDWM1001API(self):
-        """
-        Initialize dwm1001 api, by sending sending bytes
-        :param:
-        :returns: none
-        """
-        # reset incase previuos run didn't close properly
-        self.serialPortDWM1001.write(DWM1001_API_COMMANDS.RESET)
-        # send ENTER two times in order to access api
-        self.serialPortDWM1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
-        # sleep for half a second
-        time.sleep(0.5)
-        self.serialPortDWM1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
-        # sleep for half second
-        time.sleep(0.5)
-        # send a third one - just in case
-        self.serialPortDWM1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
-
 if __name__ == '__main__':  
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=str, help="Target device serial port")
     args = parser.parse_args()
     try:
-        dwm1001 = dwm1001_localizer(args)
+        dwm1001 = DWM1001DataReader(args)
         dwm1001.main()
-        #rospy.spin()
     except rospy.ROSInterruptException:
         pass
