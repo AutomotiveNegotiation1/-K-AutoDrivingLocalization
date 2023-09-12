772afaea23fa96e5a7e6097831fe002536a4ff5d
lee35460 <49274399+lee35460@users.noreply.github.com>
2023-09-12 13:34:31 +0900

    Delete src/positioning/keti_ws/build directory

--- src/positioning/keti_ws/build/dwm1001_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/keti/data/git/-K-AutoDrivingLocalization/src/positioning/keti_ws/src/dwm1001_ros
-# Build directory: /media/keti/data/git/-K-AutoDrivingLocalization/src/positioning/keti_ws/build/dwm1001_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
