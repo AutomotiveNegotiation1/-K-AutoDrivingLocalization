28f955e0a714774dc03f6574f99672a5783042a9
ljw0904 <lee35460@gmail.com>
2023-11-30 18:25:44 +0900

    [KETI] update release 1.0

---
+++ src/positioning/keti_ws/src/ipe/include/ipe/engine/fusion3/interface/_coder_fusion2_info.h
@@ -0,0 +1,25 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: _coder_fusion2_info.h
+//
+// MATLAB Coder version            : 5.4
+// C/C++ source code generated on  : 30-Nov-2023 16:43:53
+//
+
+#ifndef _CODER_FUSION2_INFO_H
+#define _CODER_FUSION2_INFO_H
+
+// Include Files
+#include "mex.h"
+
+// Function Declarations
+MEXFUNCTION_LINKAGE mxArray *emlrtMexFcnProperties();
+
+#endif
+//
+// File trailer for _coder_fusion2_info.h
+//
+// [EOF]
+//
