b0c19a3e16dbedc2c05f6053cb83220888694f64
Jae Won Lee <lee35460@gmail.com>
2024-03-28 14:42:26 +0900

    Update nfs_test_install>install.sh

--- src/positioning/nfs_test_install/install.sh
+++ src/positioning/nfs_test_install/install.sh
@@ -4,10 +4,10 @@ echo $SHELL_PATH
 sudo apt update
 sudo apt install -y nfs-kernel-server
 sudo cat /proc/fs/nfsd/versions
-mkdir -p /home/${USER}/nfs_client
-mkdir -p /home/${USER}/rosbag
+mkdir -p /mnt/nfs_client
+mkdir -p /mnt/rosbag
 cp $SHELL_PATH/rosbag/test.bag /home/${USER}/rosbag/test.bag
 IP_ADDR=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}')
-echo "/home/${USER}/nfs_client   ${IP_ADDR}/24(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/mnt/nfs_client   ${IP_ADDR}/24(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -ra
 sudo exportfs -v
