0b3ca4868f9700e8a1ac6b5423da2d66755a3444
Jae Won Lee <lee35460@gmail.com>
2024-03-28 16:57:56 +0900

    test

--- src/positioning/nfs_test_install/install.sh
+++ src/positioning/nfs_test_install/install.sh
@@ -7,6 +7,6 @@ sudo cat /proc/fs/nfsd/versions
 sudo mkdir -p /mnt/nfs_client
 sudo touch /mnt/nfs_client/logfile.log
 sudo chown ${USER}:${USER} /mnt/nfs_client/logfile.log
-echo "/mnt/nfs_client   192.168.2.127/24(rw,wdelay, root_squash, no_subtree_check, sec=sys, secure, no_all_squash)" | sudo tee -a /etc/exports
+echo "/mnt/nfs_client 192.168.2.127/24(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -ra
 sudo exportfs -v
